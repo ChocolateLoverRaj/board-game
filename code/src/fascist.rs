b685f8fc5970ed049d347b5748f7e3ba3a9d2ab9
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2026-01-09 14:42:27 -0800

    OLED displays

--- code/src/fascist.rs
+++ code/src/fascist.rs
@@ -3,14 +3,29 @@
 
 use defmt::info;
 use embassy_executor::Spawner;
+use embassy_time::Timer;
+use embedded_graphics::{
+    mono_font::{MonoTextStyleBuilder, iso_8859_16::FONT_10X20},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    text::{Baseline, Text},
+};
 use esp_backtrace as _;
 use esp_hal::{
-    interrupt::software::SoftwareInterruptControl, rmt::Rmt, time::Rate, timer::timg::TimerGroup,
+    i2c::{self, master::I2c},
+    interrupt::software::SoftwareInterruptControl,
+    rmt::Rmt,
+    time::Rate,
+    timer::timg::TimerGroup,
 };
 use esp_hal_smartled::{SmartLedsAdapterAsync, buffer_size_async, smart_led_buffer};
 use esp_println as _;
 use lib::LED_BRIGHTNESS;
 use smart_leds::{RGB8, SmartLedsWriteAsync};
+use ssd1306::{
+    I2CDisplayInterface, Ssd1306Async, prelude::DisplayRotation, prelude::*,
+    size::DisplaySize128x64,
+};
 
 esp_bootloader_esp_idf::esp_app_desc!();
 
@@ -61,6 +76,8 @@ async fn main(spawner: Spawner) {
     ];
 
     let ws2812_gpio = p.GPIO2;
+    let i2c_scl_gpio = p.GPIO0;
+    let i2c_sda_gpio = p.GPIO1;
 
     let mut buffer = smart_led_buffer!(buffer_size_async(TOTAL_LEDS));
     let mut leds_adapter = SmartLedsAdapterAsync::new(
@@ -90,4 +107,37 @@ async fn main(spawner: Spawner) {
     }
 
     leds_adapter.write(led_colors).await.unwrap();
+
+    // Turn on the OLED display
+    let i2c = I2c::new(
+        p.I2C0,
+        i2c::master::Config::default().with_frequency(Rate::from_khz(400)),
+    )
+    .unwrap()
+    .with_scl(i2c_scl_gpio)
+    .with_sda(i2c_sda_gpio)
+    .into_async();
+    let mut display = Ssd1306Async::new(
+        I2CDisplayInterface::new(i2c),
+        DisplaySize128x64,
+        DisplayRotation::Rotate0,
+    )
+    .into_buffered_graphics_mode();
+    display.init().await.unwrap();
+    let text_style = MonoTextStyleBuilder::new()
+        .font(&FONT_10X20)
+        .text_color(BinaryColor::On)
+        .build();
+    Text::with_baseline(
+        "Secret Hitler\nFascist Board",
+        Point::zero(),
+        text_style,
+        Baseline::Top,
+    )
+    .draw(&mut display)
+    .unwrap();
+    display.flush().await.unwrap();
+    // Turn off the display to not cause burn-in
+    Timer::after_secs(5).await;
+    display.set_display_on(false).await.unwrap();
 }
